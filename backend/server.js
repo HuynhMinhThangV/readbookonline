c467a640e3189c48d9528e3100d5fff6763016c8
Huynh Minh Thang <22130251@st.hcmuaf.edu.vn>
2025-03-19 23:41:55 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ import cors from "cors"
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 
 const app = express()
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
